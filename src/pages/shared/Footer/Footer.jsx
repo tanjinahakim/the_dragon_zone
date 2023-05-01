cd95e568dd53b3bc5e1714b9334d781534589235
tanjinahakim <tanjinahakim44@gmail.com>
2023-05-01 12:58:53 +0600

    header added

---
+++ src/pages/shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center text-primary'><small>copyright<span>&copy;</span>of my news</small></p>
+        </div>
+    );
+};
+
+export default Footer;
